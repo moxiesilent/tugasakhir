f92b08a25ec8c91be584a3c5053f13c645f492ec
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-05-31 00:43:23 +0700

    estimasi modal

--- app/Models/Estimasi.php
+++ app/Models/Estimasi.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Kpr extends Model
+class Estimasi extends Model
 {
     use HasFactory;
     protected $primaryKey = 'idestimasi';
