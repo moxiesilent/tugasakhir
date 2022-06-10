44ba1ade7414297329e9692fbdf6e46f1836df2b
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-06-10 23:51:44 +0700

    asdsa

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -529,10 +529,9 @@ class ApiController extends Controller
     public function deleteFotoListing(Request $request){
         $idlisting = $request->get('idlisting');
         $foto = DB::table('fotos')->where('listings_idlisting',$idlisting)->get();
-        dd($foto->path);
         foreach($foto as $f){
             // $path = 'public/images/listing'.$foto->path;
-            if(File::exists(public_path('public/images/listing'.$foto->path))){
+            if(File::exists(public_path('public/images/listing/'.$foto->path))){
                 File::delete($path);
             }
         }
