afc8e86416303735a65432e4490b29db9c762797
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-06-11 13:18:23 +0700

    asdas

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -32,29 +32,28 @@
                     <div class="col-lg-12">
                         <div class="form-container">
                             <div class="form-content">
-                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width:100%;">
-                                    <div class="carousel-inner" style="background-color:#0e1726;">
-                                    @if($foto != null)
-                                        @foreach($foto as $f => $fot)
-                                        <div class="carousel-item {{$f == 0 ? 'active' : '' }}">
-                                            <img class="" src="{{asset('images/listing/'.$fot->path)}}" height='350px'>
+                                @if($foto != "")
+                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width:100%;">
+                                        <div class="carousel-inner" style="background-color:#0e1726;">
+                                            @foreach($foto as $f => $fot)
+                                            <div class="carousel-item {{$f == 0 ? 'active' : '' }}">
+                                                <img class="" src="{{asset('images/listing/'.$fot->path)}}" height='350px'>
+                                            </div>
+                                            @endforeach
                                         </div>
-                                        @endforeach
-                                    @else
-                                        <div class="carousel-item active">
-                                            <img class="" src="{{asset('images/listing/default.png')}}" height='350px'>
-                                        </div>
-                                    @endif
+                                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
                                     </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </div><br>
+                                    @else
+                                                <img class="" src="{{asset('images/listing/default.png')}}" height='350px'>
+                                @endif    
+                                <br>
                                 <div class="row">
                                     <h4><b>{{$listing->judul}}</b></h4>
                                 </div>
