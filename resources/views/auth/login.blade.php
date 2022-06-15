ff6ec7cb6e3d86185e21c3c882707c5a258499db
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-06-15 23:05:28 +0700

    asdas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,6 +50,12 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                                     <input id="password" name="password" type="password" class="form-control" placeholder="Password" required>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                                    
+                                    <p class="signup-link" style="color: red; margin-top: 2%; margin-bottom: -2%">
+                                        @if(count($errors) > 0)
+                                        Email atau password salah
+                                        @endif
+                                    </p>
                                 </div>
                                 <div class="d-sm-flex justify-content-between">
                                     <div class="field-wrapper">
