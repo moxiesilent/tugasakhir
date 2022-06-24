56d7d7e2dbf64c7749f09345138f854eeb18bc42
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-06-24 12:38:13 +0700

    asdas

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -168,7 +168,7 @@
                                 <td>{{number_format($d->komisi_agen_penjual)}}</td>
                                 <td>
                                     <div class="dropdown">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink2" data-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="true">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink2"data-boundary="window" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
                                         </a>
                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink2">
