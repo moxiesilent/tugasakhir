b563541c93cdc09a4d9fb790a64b3552d715ae3e
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-06-24 12:49:27 +0700

    asdas

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -167,11 +167,11 @@
                                 <td>{{number_format($d->komisi_agen_pemilik)}}</td>
                                 <td>{{number_format($d->komisi_agen_penjual)}}</td>
                                 <td>
-                                    <div class="dropdown">
+                                    <div class="dropdown" style="Z-index:10;">
                                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
                                         </a>
-                                        <div class="dropdown-menu" style="Z-index:10;" aria-labelledby="dropdownMenuLink2">
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink2">
                                             <a href="{{url('laporans/'.$d->idlaporan)}}"><button class="dropdown-item btn btn-primary">&nbsp&nbsp&nbspDetail</button></a><br>
                                             <a href="{{url('laporans/'.$d->idlaporan.'/edit')}}"><button class="dropdown-item btn btn-warning">&nbsp&nbsp&nbspUbah</button></a><br>
                                             <button class="dropdown-item btn btn-danger" onclick="hapus('{{csrf_token()}}','{{$d->idlaporan}}')">&nbsp&nbsp&nbspHapus</button>
