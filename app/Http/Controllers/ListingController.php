68154ebf24477f8e48a6c5efde49c15590da5c23
MATTHEW NOTO SISWO _ <s160418071@student.ubaya.ac.id>
2022-07-23 11:35:24 +0700

    asdas

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -74,8 +74,8 @@ class ListingController extends Controller
             else{
                 $data = new Listing();
 
-                $kodeAgen = User::select('kode')->where('idagen', $request->get('idagen'))->first();
-                dd($kodeAgen);
+                $dataAgen = User::select('kode')->where('idagen', $request->get('idagen'))->first();
+                $kodeAgen = $dataAgen['kode'];
                 $lastKode = Listing::select('kode_listing')->where('kode_listing','LIKE','%'.$kodeAgen.'%')->latest()->first();
                 $lastNumber = substr($lastKode['kode_listing'],5);
                 $angkaKode = str_pad($lastNumber + 1, 3, "0", STR_PAD_LEFT);
